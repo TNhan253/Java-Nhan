f8dea3896109f3b6e53b7bf08f24f10d4a1fba62
TNhan253 <tuyetnhan253@gmail.com>
2024-09-24 23:40:26 +0700

    Làm bài tập nhập xuất
    Làm bài tập nhập xuất

---
+++ NhapTuBanPhim/src/Nhap/NhapTuBanPhim.java
@@ -0,0 +1,28 @@
+package Nhap;
+
+import java.util.Scanner;
+
+public class NhapTuBanPhim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập họ tên:");
+		String hoTen = scanner.nextLine();
+		System.out.print("Tuổi: ");
+		int tuoi = scanner.nextInt();
+		System.out.print("Cân nặng (Kg): ");
+		double canNang = scanner.nextDouble();
+		System.out.print("Chiều cao (cm): ");
+		double chieuCao = scanner.nextDouble();
+		//Xuất thông tin nhập
+		System.out.println("------------------------------------------");
+		System.out.println("Họ và tên: " + hoTen);
+		System.out.println("Tuổi: " + tuoi);
+		System.out.println("Cân nặng: " + canNang);
+		System.out.println("Chiều cao: " + chieuCao);
+			}
+
+	}
+
+
---
+++ NhapTuBanPhim/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module NhapTuBanPhim {
+}
