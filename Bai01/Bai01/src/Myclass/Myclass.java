926a2483811b34e98e47eea28ce0ad728c5e8659
TNhan253 <tuyetnhan253@gmail.com>
2024-09-24 22:26:45 +0700

    Cập nhật
    Cập nhật bài 1

---
+++ Bai01/Bai01/src/Myclass/Myclass.java
@@ -0,0 +1,14 @@
+package Myclass;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		int a =5;
+		int b = 7;
+		int c = a+ b;
+		System.out.println("HÔM NAY TÔI BẮT ĐẦU HỌC JAVA");
+		System.out.println("Tổng là: " + c);
+
+	}
+
+}
